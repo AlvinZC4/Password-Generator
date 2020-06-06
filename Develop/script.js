ea29f2ad3deced233738972af05c731b8a981303
Alvin <alvinzcoxiv@gmail.com>
2020-06-05 23:44:29 -0400

    wrote chatacter length and character type gates to ensure a valid password can be generated

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,22 @@
+var passwordLength = prompt("Enter how long you would like your password to be.  It must be between 8 and 128 characters")
+  
+  while (passwordLength < 8 || passwordLength > 128) {
+    var passwordLength = prompt("Invalid entry!  Password must be between 8 and 128 characters!")
+  }
+
+var lowerCase = confirm("Would you like to include lower case characters?")
+var upperCase = confirm("Would you like to include upper case charactets?")
+var numbers = confirm("Would you like to include numerical characters?")
+var special = confirm("Would you like to include special characters?")
+
+  while (lowerCase == false && upperCase == false && numbers == false && special == false) {
+    alert("Your password must include at least one of the following: lower case, upper case, numerical, or special characters!")
+    var lowerCase = confirm("Would you like to include lower case characters?")
+    var upperCase = confirm("Would you like to include upper case charactets?")
+    var numbers = confirm("Would you like to include numerical characters?")
+    var special = confirm("Would you like to include special characters?")
+  }
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
