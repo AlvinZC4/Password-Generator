ffdb7eb5f4d78f44bd6f44070904384fe2797835
Alvin <alvinzcoxiv@gmail.com>
2020-06-07 00:23:01 -0400

    Added prompts that will allow user input for creating password

--- Develop/script.js
+++ Develop/script.js
@@ -9,13 +9,64 @@ var upperCase = confirm("Would you like to include upper case charactets?")
 var numbers = confirm("Would you like to include numerical characters?")
 var special = confirm("Would you like to include special characters?")
 
-  while (lowerCase == false && upperCase == false && numbers == false && special == false) {
-    alert("Your password must include at least one of the following: lower case, upper case, numerical, or special characters!")
+  while (lowerCase == false && upperCase == false) {
+    alert("Your password must include either lower case or upper case characters!")
     var lowerCase = confirm("Would you like to include lower case characters?")
     var upperCase = confirm("Would you like to include upper case charactets?")
     var numbers = confirm("Would you like to include numerical characters?")
     var special = confirm("Would you like to include special characters?")
   }
+
+  var passwordCriteria = {
+    "favFoods": [
+      prompt("What is one of your favorite foods (1 of 5)"),
+      prompt("What is another one of your favorite foods (2 of 5)"),
+      prompt("What is another one of your favorite foods (3 of 5)"),
+      prompt("What is another one of your favorite foods (4 of 5)"),
+      prompt("What is another one of your favorite foods (5 of 5)")
+    ],
+    "favShows": [
+      prompt("What is one of your favorite shows (1 of 5)"),
+      prompt("What is another one of your favorite shows (2 of 5)"),
+      prompt("What is another one of your favorite shows (3 of 5)"),
+      prompt("What is another one of your favorite shows (4 of 5)"),
+      prompt("What is another one of your favorite shows (5 of 5)")
+    ],
+    "favMovies": [
+      prompt("What is one of your favorite movies (1 of 5)"),
+      prompt("What is another one of your favorite movies (2 of 5)"),
+      prompt("What is another one of your favorite movies (3 of 5)"),
+      prompt("What is another one of your favorite movies (4 of 5)"),
+      prompt("What is another one of your favorite movies (5 of 5)")
+    ],
+    "firstPetName": prompt("What was the name of your first pet?"),
+    "birthCity": prompt("What city were you born in?"),
+    "favoriteColor": prompt("What is your favorite color?"),
+    "motherMaiden": prompt("What was your mother's madian name?"),
+    "birthYear": prompt("What year were you born?")
+  }
+
+  // while (typeof passwordCriteria.birthYear !== "number" || passwordCriteria.birthYear.length !== 4) {
+  //   passwordCriteria.birthYear = prompt("Invalid Entry: What year were you born?");
+  // }
+
+function consoleArrCheck (arrCheck) {
+  console.log(arrCheck[1]),
+  console.log(arrCheck[2]),
+  console.log(arrCheck[3]),
+  console.log(arrCheck[4]),
+  console.log(arrCheck[5])
+}
+
+consoleArrCheck (passwordCriteria.favFoods)
+consoleArrCheck (passwordCriteria.favShows)
+consoleArrCheck (passwordCriteria.favMovies)
+
+console.log(passwordCriteria.firstPetName)
+console.log(passwordCriteria.birthCity)
+console.log(passwordCriteria.favoriteColor)
+console.log(passwordCriteria.motherMaiden)
+console.log(passwordCriteria.birthYear)
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
